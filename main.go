b432e1732f52384cb1ec1c82ecb694736c4a7ab2
Somtochukwu Francis <somtofrancis5@gmail.com>
2026-02-11 01:54:07 +0100

    Temporal Worker Setup

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/SomtoJF/iris-worker/initializers/sqldb"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+type TaskQueueName string
+
+const (
+	JobApplicationTaskQueueName TaskQueueName = "job-application"
 )
 
 func init() {
@@ -15,5 +22,30 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	c, err := client.Dial(client.Options{})
+
+	if err != nil {
+		log.Fatalln("Unable to create Temporal client:", err)
+	}
+
+	defer c.Close()
+
+	w := worker.New(c, string(JobApplicationTaskQueueName), worker.Options{})
+
+	registerJobApplicationWorkflows(w)
+	registerJobApplicationActivities(w)
+
+	// Start listening to the Task Queue.
+	err = w.Run(worker.InterruptCh())
+	if err != nil {
+		log.Fatalln("unable to start Worker", err)
+	}
+}
+
+func registerJobApplicationWorkflows(w worker.Worker) {
+	// w.RegisterWorkflow(JobApplicationWorkflow)
+}
+
+func registerJobApplicationActivities(w worker.Worker) {
+	// w.RegisterActivity(JobApplicationActivity)
 }
